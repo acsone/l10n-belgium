1691efe513fe36549a64ccc722ca5508f9fac1be
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-11-28 17:39:21 +0100

    l10n_be_coa_multilang(_detail): normalize version numbers

--- l10n_be_coa_multilang_detail/__openerp__.py
+++ l10n_be_coa_multilang_detail/__openerp__.py
@@ -35,7 +35,7 @@
     # Categories can be used to filter modules in modules listing
     # Check http://goo.gl/0TfwzD for the full list
     'category': 'Localization/Account Charts',
-    'version': '0.1',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
